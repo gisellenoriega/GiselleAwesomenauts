97f0242e3fc7132426149ca43ef2e752d9b1d835
Giselle Noriega <gnoriega626@gmail.com>
2015-01-28 11:49:35 -0800

    awesomenauts
    video 10, animated my player

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,6 +1,6 @@
 // BASIC PLAYER CLASS
 // game.PlayerEntity both letters get to be capital because it it a class
-// init: function is our fucntion constructor
+// init: function is our function constructor
 // init: function initializes player
 // this._super means reaching to the constructor of the entity
 // spriteheight and spritewidth are telling us what the size of the image is
@@ -18,24 +18,39 @@ game.PlayerEntity = me.Entity.extend({
 				}
 			}]);
 
-			this.body.setVelocity(5, 0);
+			this.body.setVelocity(5, 20);
 
+			this.renderable.addAnimation("idle", [78]);
+			this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 123, 124, 125], 80);
+
+			this.renderable.setCurrentAnimation("idle");
 	},
 
 	update: function(delta){
-		if(me.input.isKeyPressed("right")){
+			if(me.input.isKeyPressed("right")){
 			// adds the pos. of my x by adding the velocity defined above in
 			// setVelocity() and multiplying it by me.timer.tick
 			// me.timer.tick makes the movement smooth
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.flipX(true);
 		}
 		else{
 			this.body.vel.x = 0;
 		}
-
-		this.body.vel.update(delta);
-		return true;
-		// makes this all work.
-		// Delta is the change in time it happened
+   if(this.body.vel.x !== 0) {
+	if(!this.renderable.isCurrentAnimation("walk")){
+		this.renderable.setCurrentAnimation("walk");
 	}
+	}else{
+		this.renderable.setCurrentAnimation("idle");
+	}
+
+
+
+
+	this.body.update(delta);
+
+	this._super(me.Entity, "update", [delta]);
+	return true;
+}
 });
